79cd8d5d814990339faa62f21b3c54463ad45134
cyrille <jeremie.cyrille@icloud.com>
2023-08-17 15:04:10 +0200

    fix: eslint

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React from 'react';
+import Link from 'next/link';
 
 export default function Header() {
   const [navbarOpen, setNavbarOpen] = React.useState(false);
@@ -7,14 +8,14 @@ export default function Header() {
     <div className="fixed top-0 z-30 w-full transition duration-300 ease-in-out clearNav md:bg-opacity-90">
       <div className="flex flex-col max-w-6xl px-4 mx-auto md:items-center md:justify-between md:flex-row md:px-6 lg:px-8">
         <div className="flex flex-row items-center justify-between p-4">
-          <a
+          <Link
             href="/"
             className="text-lg font-semibold tracking-widest rounded-lg focus:outline-none focus:shadow-outline"
           >
             <h1 className="text-4xl tracking-tighter text-green-400 Avenir md:text-4x1 lg:text-3xl">
               Fidecly
             </h1>
-          </a>
+          </Link>
           <button
             className="px-3 py-1 leading-none text-white outline-none cursor-pointer md:hidden focus:outline-none "
             type="button"
@@ -41,30 +42,30 @@ export default function Header() {
         </div>
         <div
           className={
-            "md:flex flex-grow items-center" +
-            (navbarOpen ? " flex" : " hidden")
+            'md:flex flex-grow items-center' +
+            (navbarOpen ? ' flex' : ' hidden')
           }
         >
           <nav className="flex-col flex-grow ">
             <ul className="flex flex-wrap items-center justify-end flex-grow">
               <li>
-                <a
+                <Link
                   href="/"
                   className="flex items-center px-5 py-3 font-medium text-gray-600 transition duration-150 ease-in-out hover:text-gray-900"
                 >
                   Accueil
-                </a>
+                </Link>
               </li>
               <li>
-                <a
+                <Link
                   href="/"
                   className="flex items-center px-5 py-3 font-medium text-gray-600 transition duration-150 ease-in-out hover:text-gray-900"
                 >
                   A propos de nous
-                </a>
+                </Link>
               </li>
               <li>
-                <a
+                <Link
                   className="inline-flex items-center px-4 py-2 mt-2 font-medium text-gray-600 transition duration-500 ease-in-out transform bg-transparent bg-green-400 rounded-lg text-md md:mt-0 md:ml-4 hover:text-gray-900"
                   href="/"
                 >
@@ -79,7 +80,7 @@ export default function Header() {
                       fillRule="nonzero"
                     />
                   </svg>
-                </a>
+                </Link>
               </li>
             </ul>
           </nav>
